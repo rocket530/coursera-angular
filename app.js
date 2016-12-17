5961d83d94f57c46ee2bc40fbf81e75ac8c1ad45
Aamir <aamirusmani82@gmail.com>
2016-12-17 11:08:06 +0500

    Files to commit

---
+++ app.js
@@ -0,0 +1,105 @@
+﻿var student = {
+
+    name: "",
+    type: "student"
+
+};
+
+
+document.addEventListener('DOMContentLoaded', contentLoaded);
+
+function contentLoaded(event) {
+
+    document.getElementById('name').addEventListener("keyup", keyUp);
+
+}
+
+function keyUp(event) {
+
+    calculateNumericOutput(); 
+}
+
+function calculateNumericOutput() {
+
+    student.name = document.getElementById('name').value;
+
+    var totalNameValue = 0;
+
+    for (var i = 0; i < student.name.length ; i++) {
+
+        totalNameValue += student.name.charCodeAt(i);
+
+    }
+
+
+    var output = "Total Numeric value of person name is " + totalNameValue;
+    document.getElementById('output').innerText = output; 
+
+}
+
+
+(function () {
+
+    'use strict';
+
+    angular.module('app', []).
+    controller('NameCalculateController',DIController);
+
+
+    DIController.$inject = ['$scope', '$injector'];
+
+
+    function DIController ($scope, $injector) {
+
+        $scope.name = "";
+        $scope.total = 0;
+        $scope.stateOfBeing = "hungry";
+
+        
+
+        $scope.displayNumeric = function () {
+
+            $scope.total = calculateName($scope.name);
+
+        };
+
+        $scope.feedAamir = function () {
+
+            $scope.stateOfBeing = "fed";
+        }
+
+   
+        console.log($injector.annotate(DIController));
+
+    }
+
+
+
+    
+
+
+
+
+    function calculateName(name) {
+
+        var totalNameValue = 0;
+
+        for (var i = 0; i < name.length ; i++) {
+
+            totalNameValue += name.charCodeAt(i);
+
+        }
+
+        return totalNameValue;
+
+    }
+
+
+
+   
+
+
+
+
+
+})();
---
+++ module1-solution/scripts/app.js
@@ -0,0 +1,64 @@
+﻿(function () {
+
+
+    angular.module('app', []).controller('LunchController', LunchController);
+
+    LunchController.$inject = ['$scope'];
+
+    function LunchController($scope) {
+
+        $scope.message = "";
+        $scope.list = "";
+        $scope.class = "";
+
+        $scope.checkTooMuch = function () {
+
+            var arrList = $scope.list.split(',').clean("").clean(" ");
+
+
+            if (arrList.length <= 0) {
+
+                 $scope.message = "Please enter data first."
+                 $scope.class = 'invalid';
+            } else {
+
+              
+
+                if (arrList.length <= 3) {
+
+                    $scope.message = "Enjoy!"
+                    $scope.class = "valid";
+
+                } else if (arrList.length > 3) {
+
+                    $scope.message = "Too much!";
+                    $scope.class = "valid";
+
+                }
+
+
+
+            }
+
+
+        };
+
+
+    }
+
+
+    Array.prototype.clean = function (deleteValue) {
+        for (var i = 0; i < this.length; i++) {
+            if (this[i] == deleteValue) {
+                this.splice( i, 1);
+                i--;
+            }
+        }
+        return this;
+    };
+
+
+
+
+
+}());

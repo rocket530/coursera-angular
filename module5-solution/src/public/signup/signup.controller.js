c0064b5411213d7eb2589377d735aa9ae044fa65
Aamir <aamirusmani82@gmail.com>
2017-01-06 02:23:36 +0500

    This is module 5 solution

---
+++ module5-solution/src/public/signup/signup.controller.js
@@ -0,0 +1,65 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('public').
+    controller('SignupController', SignupController);
+
+    SignupController.$inject = ["UserService",'$rootScope'];
+
+    function SignupController(UserService,$rootScope) {
+        var ctrl = this;
+        ctrl.formFilled = false;
+        ctrl.saveUser = function (user) {
+            ctrl.formFilled = true;
+            UserService.setUser(user);
+        };
+
+        ctrl.getUser = function () {
+            ctrl.user = UserService.getUser();
+        }
+
+        ctrl.checkMenuItem = function (menuItem) {
+
+            ctrl.menuItem = null;
+            UserService.getMenuItem(menuItem).then(function (response) {
+
+                ctrl.menuItem = response;
+
+                if (!ctrl.menuItem) {
+                    ctrl.signup.menuNumber.$setValidity('required', false);   
+                } else {
+                    ctrl.signup.menuNumber.$setValidity('required', true);
+                }
+
+            });
+
+            
+            
+
+        }
+
+
+
+    }
+
+
+
+    angular.module('public').
+    controller('MyinfoController', MyinfoController);
+    MyinfoController.$inject = ["user","ApiPath"];
+    function MyinfoController(user, ApiPath) {
+        var ctrl = this;
+
+        ctrl.user = user;
+        ctrl.basePath = ApiPath;
+
+
+    }
+
+
+
+
+
+
+}());
---
+++ module5-solution/src/public/signup/user.service.js
@@ -0,0 +1,64 @@
+﻿(function () {
+    
+    'use strict';
+
+    
+
+    angular.module('public')
+    .service('UserService', UserService);
+
+    UserService.$inject = ["$http", "ApiPath"];
+
+    function UserService($http, ApiPath) {
+        var service = this;
+
+        var _user = {};
+        var _menuItem = {};
+
+        service.setUser = function (user) {
+            user.formFilled = true;
+            _user = user;
+        };
+
+        service.getUser = function () {
+            _user.menuItem = _menuItem;
+            return _user;
+
+        };
+
+        service.getMenuItem = function (menuItem) {
+          
+            return $http.get(ApiPath + '/menu_items.json').then(function (response) {
+                var menuitems = response.data.menu_items;
+
+                if(menuItem){
+                for (var i = 0; i < menuitems.length; i++) {
+
+                    if (menuitems[i].short_name.toLowerCase() === menuItem.toLowerCase()) {
+                        _menuItem = menuitems[i];
+                        return menuitems[i];
+                    }
+
+                }
+                }
+
+            });
+            
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}());
